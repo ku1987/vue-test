5026b68902b00b814d13d8051ce883640367a95a
ku1987 <kei.usami1987@gmail.com>
2020-07-06 22:53:03 +0900

    Fix linter

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "plugin:vue/vue3-recommended"
+  ],
+  "rules": {
+    "semi": ["error", "always"],
+    "semi-style": ["error", "last"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error",
+    "eslint-disable-next-line": 0,
+    "vue/max-attributes-per-line": 0
+  }
+};
